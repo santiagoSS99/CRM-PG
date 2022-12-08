1633e9f4918b097e09dc549b8562b596f259d1d5
santiagoSS99 <santiagoxq@icloud.com>
2022-12-08 02:34:51 -0500

    scaffolding with services folder

--- crm-front/src/app/app-routing.module.ts
+++ crm-front/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { InventoryComponent } from './pages/inventory/inventory.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  // {path: '', redirectTo:'home',pathMatch:'full'}
+  { path: '/inventory', component: InventoryComponent }
 ];
 
 @NgModule({
