5d73c1959708bb3c5dfbfeee22eb2fd0383fa00b
santiagoSS99 <santiagoxq@icloud.com>
2023-01-07 19:44:15 -0500

    endpoint already to upload images and save in fs

--- crm-back/src/files/files.controller.ts
+++ crm-back/src/files/files.controller.ts
@@ -1,10 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { FilesService } from './files.service';
+import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException } from '@nestjs/common';
+import { UploadedFile, UseInterceptors } from '@nestjs/common/decorators';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { fileFilter } from './helpers/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helpers/fileNamer.helper';
 
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) { }
 
-
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file', {
+    fileFilter: fileFilter,
+    storage: diskStorage({
+      destination: './static/products',
+      filename: fileNamer
+    })
+  }))
+  uploadImageFile(
+    @UploadedFile()
+    file: Express.Multer.File) {
+    if (!file) throw new BadRequestException('File')
+    return {
+      fileName: file.originalname
+    }
+  }
 }
