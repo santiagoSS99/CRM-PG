2ba01657b2881fb709d00d990b250777916c6708
santiagoSS99 <santiagoxq@icloud.com>
2023-03-08 18:26:29 -0500

    cahnges in sales component and create POST method to create orders with table assigned

--- crm-back/src/orders/orders.service.ts
+++ crm-back/src/orders/orders.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Tables } from 'src/tables/entities/table.entity';
+import { Repository } from 'typeorm';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { Order } from './entities/order.entity';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+
+  constructor(
+    @InjectRepository(Order)
+    private orderRepository: Repository<Order>,
+    @InjectRepository(Tables)
+    private tableRepository: Repository<Tables>,) { }
+
+  // create order with table assigned
+  async create(tableId, order): Promise<Order> {
+    const table = await this.tableRepository.findOne(tableId)
+    order.table = table
+    return this.orderRepository.save(order);;
   }
 
   findAll() {
