a84ba9c662cb2206357aa1466fae17c8ad04ac36
santiagoSS99 <santiagoxq@icloud.com>
2023-05-06 15:59:13 -0500

    Service to create customer by admin

--- crm-front/src/app/services/customer.service.ts
+++ crm-front/src/app/services/customer.service.ts
@@ -23,4 +23,8 @@ export class CustomerService {
   createCustomer(Customer: Customer): Observable<Customer> {
     return this.http.post<Customer>(`${this.BASE_URL}/customer/register`, Customer)
   }
+
+  createCustomerFromAdmin(Customer: Customer): Observable<Customer> {
+    return this.http.post<Customer>(`${this.BASE_URL}/customer`, Customer)
+  }
 }
