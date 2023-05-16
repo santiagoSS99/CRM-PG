4bc2569dea5c488655b1efbf3842b11c78454049
santiagoSS99 <santiagoxq@icloud.com>
2023-05-16 16:58:45 -0500

    User can get calls to display in the front

--- crm-front/src/app/customer/prospection/customer-calls/customer-calls.component.ts
+++ crm-front/src/app/customer/prospection/customer-calls/customer-calls.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CustomerService } from 'src/app/services/customer.service';
+declare var $: any
+
 
 @Component({
   selector: 'app-customer-calls',
@@ -12,21 +15,98 @@ export class CustomerCallsComponent implements OnInit {
   call: any = {
     result: '',
     date: new Date().toISOString().substring(0, 10),
-    hour: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-  }
-  public time = { hour: new Date().getHours(), minute: new Date().getMinutes() };
+    hour: '',
+    assesor: localStorage.getItem('id'),
+    customer: '',
+    note: ''
+  };
+  time = { hour: new Date().getHours(), minutes: new Date().getMinutes() }
   public token = localStorage.getItem('token');
+  btn_load = false
+
+  calls: any = {}
+
+
 
   constructor(
     private _route: ActivatedRoute,
+    private customerService: CustomerService
   ) { }
 
   ngOnInit(): void {
+    this.getCalls();
     this._route.params.subscribe(
       params => {
         this.id = parseInt(params['id'], 10)
+        this.getCallsByCustomer(this.id)
       }
     )
   }
 
+  registerCall() {
+
+    if (this.time || this.time != undefined || this.time != null) {
+      this.call.hour = this.time.hour + ':' + this.time.minutes
+    }
+
+    if (!this.call.result) {
+      $.notify('Please fill result field', {
+        type: 'danger',
+        spacing: 10,
+        timer: 2000,
+        placement: {
+          from: 'top',
+          align: 'right'
+        },
+        delay: 1000,
+        animate: {
+          enter: 'animated ' + 'bounce',
+          exit: 'animated ' + 'bounce'
+        }
+      });
+    } else {
+      this.btn_load = true
+      this.call.customer = this.id.toString()
+      this.customerService.createCustomerCall(this.call, this.token).subscribe(
+        res => {
+          this.btn_load = false
+          this.closeModal()
+          $.notify(res, {
+            type: 'success',
+            spacing: 10,
+            timer: 2000,
+            placement: {
+              from: 'top',
+              align: 'right'
+            },
+            delay: 1000,
+            animate: {
+              enter: 'animated ' + 'bounce',
+              exit: 'animated ' + 'bounce'
+            }
+          });
+        })
+    }
+  }
+
+  getCalls() {
+    this.customerService.getCalls(this.token).subscribe(
+      res => {
+        console.log(res)
+      })
+  }
+
+  getCallsByCustomer(id: any) {
+    id = this.id
+    console.log(id)
+    this.customerService.getCallsByCustomer(id, this.token).subscribe(
+      res => {
+        console.log(res)
+      })
+  }
+
+  closeModal() {
+    $(`#modalLlamada`).modal('hide');
+  }
 }
+
