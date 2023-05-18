149c04d33b6adc183edd0ce72bc6b87c2f7c6af8
santiagoSS99 <santiagoxq@icloud.com>
2023-05-17 20:56:43 -0500

    Backend logic to create / send email

--- crm-back/src/customer-mail/customer-mail.module.ts
+++ crm-back/src/customer-mail/customer-mail.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CustomerMailService } from './customer-mail.service';
 import { CustomerMailController } from './customer-mail.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerMail } from './entities/customer-mail.entity';
+import { Customer } from 'src/customer/entities/customer.entity';
 
 @Module({
   controllers: [CustomerMailController],
-  providers: [CustomerMailService]
+  providers: [CustomerMailService],
+  imports: [TypeOrmModule.forFeature([CustomerMail, Customer])]
 })
-export class CustomerMailModule {}
+export class CustomerMailModule { }
--- crm-back/src/customer-mail/dto/create-customer-mail.dto.ts
+++ crm-back/src/customer-mail/dto/create-customer-mail.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCustomerMailDto {}
+import { IsString } from "class-validator";
+import { User } from "src/auth/entities/user.entity";
+import { Customer } from "src/customer/entities/customer.entity";
+
+export class CreateCustomerMailDto {
+    @IsString()
+    subject: string
+
+    @IsString()
+    content: string
+
+    @IsString()
+    assesor: User
+
+    @IsString()
+    customer: Customer
+}
