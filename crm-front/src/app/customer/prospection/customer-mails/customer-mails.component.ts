8ba36d9c6fefc1b6c96a735b7e4a3e1e7ca02bfa
santiagoSS99 <santiagoxq@icloud.com>
2023-05-19 00:09:28 -0500

    :sparkles: (UX): User can view all mail data sended to customer

--- crm-front/src/app/customer/prospection/customer-mails/customer-mails.component.ts
+++ crm-front/src/app/customer/prospection/customer-mails/customer-mails.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CustomerService } from 'src/app/services/customer.service';
 
+declare var $: any
+
+
 @Component({
   selector: 'app-customer-mails',
   templateUrl: './customer-mails.component.html',
@@ -14,12 +17,13 @@ export class CustomerMailsComponent implements OnInit {
   data = false
   load_data = true
 
-  // editorconfig = {
-  //   base_url: '/tinymce',
-  //   suffix: '.min',
-  //   plugins: 'link list image table wordcount'
-  // }
+  sendBtn = false
+  email: any = {
+    assesor: localStorage.getItem('id'),
+    customer: '',
+  }
 
+  mails: any = []
 
 
   constructor(
@@ -38,7 +42,7 @@ export class CustomerMailsComponent implements OnInit {
             if (response != undefined) {
               this.data = true;
               this.load_data = false;
-              this.initData()
+              this.getMailsByCustomer(this.id)
             }
           }
         )
@@ -50,7 +54,96 @@ export class CustomerMailsComponent implements OnInit {
     // });
   }
 
-  initData() {
+  sendEmail() {
+    console.log(this.email)
+    if (!this.email.content) {
+      $.notify('Ingrese el contenido del correo', {
+        type: 'danger',
+        spacing: 10,
+        timer: 2000,
+        placement: {
+          from: 'top',
+          align: 'right'
+        },
+        delay: 1000,
+        animate: {
+          enter: 'animated ' + 'bounce',
+          exit: 'animated ' + 'bounce'
+        }
+      });
+    } else if (!this.email.subject) {
+      $.notify('Ingrese el asunto del correo', {
+        type: 'danger',
+        spacing: 10,
+        timer: 2000,
+        placement: {
+          from: 'top',
+          align: 'right'
+        },
+        delay: 1000,
+        animate: {
+          enter: 'animated ' + 'bounce',
+          exit: 'animated ' + 'bounce'
+        }
+      });
+    } else {
+      this.sendBtn = true
+      this.email.customer = this.id.toString()
+      this.customerService.createCustomerMail(this.email, this.token).subscribe(
+        response => {
+          this.closeModal()
+          console.log(response);
+          this.sendBtn = false
+          $.notify(response, {
+            type: 'success',
+            spacing: 10,
+            timer: 2000,
+            placement: {
+              from: 'top',
+              align: 'right'
+            },
+            delay: 1000,
+            animate: {
+              enter: 'animated ' + 'bounce',
+              exit: 'animated ' + 'bounce'
+            }
+          });
+        }
+      )
+    }
+  }
 
+  getMails() {
+    this.customerService.getCalls(this.token).subscribe(
+      res => {
+        console.log(res)
+      })
+  }
+
+  getMailsByCustomer(id: any) {
+    id = this.id
+    console.log(id)
+    this.customerService.getMailsByCustomer(id, this.token).subscribe(
+      res => {
+        this.mails = res
+        console.log(res)
+      })
+  }
+
+
+  closeModal() {
+    $(`#modalMail`).modal('hide');
+  }
+
+  toggleEmail(id: any) {
+    var clase = $('#card_' + id).attr('class');
+
+    if (clase == 'card-spacer-x pt-2 pb-5 toggle-off-item') {
+      $('#card_' + id).removeClass('toggle-off-item');
+      $('#card_' + id).addClass('toggle-on-item');
+    } else if (clase == 'card-spacer-x pt-2 pb-5 toggle-on-item') {
+      $('#card_' + id).removeClass('toggle-on-item');
+      $('#card_' + id).addClass('toggle-off-item');
+    }
   }
 }
