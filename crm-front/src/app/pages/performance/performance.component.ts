cde9622144247d5b64cacafa745741f80cf59bc1
santiagoSS99 <santiagoxq@icloud.com>
2023-05-21 11:56:24 -0500

    :sparkles: Feat(Performance bussies): Chart to display sales per month

--- crm-front/src/app/pages/performance/performance.component.ts
+++ crm-front/src/app/pages/performance/performance.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js';
+import { PurchaseService } from 'src/app/services/purchase.service';
+import 'chart.js/auto';
+// import 'chartjs-adapter-chartjs/dist/chartjs-adapter-chart.js';
+// 
 declare var ApexCharts: any
 
 @Component({
@@ -8,17 +13,51 @@ declare var ApexCharts: any
 })
 export class PerformanceComponent implements OnInit {
 
+  token = localStorage.getItem('token')
   year: any
   month: any
-  constructor() { }
+  dataChart: any
+  constructor(
+    private purchaseService: PurchaseService
+  ) { }
 
   ngOnInit(): void {
+    this.loadTotalAmountPurchase()
   }
 
   initPerformanceChart() {
 
   }
 
+  loadTotalAmountPurchase() {
+    this.purchaseService.getDataAmountToDash(this.token).subscribe(
+      res => {
 
+        if (res) {
+          this.dataChart = document.getElementById('myChart1');
+          new Chart(this.dataChart, {
+            type: 'line',
+            data: {
+              labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dec'],
+              datasets: [{
+                label: 'Total ',
+                data: res.data,
+                borderWidth: 1
+              }]
+            },
+            options: {
+              // scales: {
+              //   y: {
+              //     beginAtZero: true
+              //   }
+              // }
+            }
+          });
+        }
+
+        console.log(res)
+        console.log(res.data)
+      })
+  }
 
 }
