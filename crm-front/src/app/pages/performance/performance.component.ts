a27359d01f473daade094e46985df68dadaf0b32
santiagoSS99 <santiagoxq@icloud.com>
2023-05-23 00:20:39 -0500

    New dashboard to display products movements

--- crm-front/src/app/pages/performance/performance.component.ts
+++ crm-front/src/app/pages/performance/performance.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
 import { PurchaseService } from 'src/app/services/purchase.service';
 import 'chart.js/auto';
+import { ProductService } from 'src/app/services/product.service';
 // import 'chartjs-adapter-chartjs/dist/chartjs-adapter-chart.js';
 // 
 declare var ApexCharts: any
@@ -16,13 +17,17 @@ export class PerformanceComponent implements OnInit {
   token = localStorage.getItem('token')
   year: any
   month: any
-  dataChart: any
+  dataChartToShowAmount: any
+  dataChartToShowProductsBySales: any
+  productsLabels: Array<any> = []
   constructor(
-    private purchaseService: PurchaseService
+    private purchaseService: PurchaseService,
+    private productService: ProductService,
   ) { }
 
   ngOnInit(): void {
-    this.loadTotalAmountPurchase()
+    this.loadTotalAmountPurchase();
+    this.productsWithSales()
   }
 
   initPerformanceChart() {
@@ -34,8 +39,8 @@ export class PerformanceComponent implements OnInit {
       res => {
 
         if (res) {
-          this.dataChart = document.getElementById('myChart1');
-          new Chart(this.dataChart, {
+          this.dataChartToShowAmount = document.getElementById('myChart1');
+          new Chart(this.dataChartToShowAmount, {
             type: 'line',
             data: {
               labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dec'],
@@ -57,6 +62,37 @@ export class PerformanceComponent implements OnInit {
 
         console.log(res)
         console.log(res.data)
+      })
+  }
+
+  productsWithSales() {
+    this.productService.productsWithSales(this.token).subscribe(
+      res => {
+        console.log(res.data.productNames);
+        this.productsLabels = res
+
+        if (res) {
+          this.dataChartToShowProductsBySales = document.getElementById('myChart3');
+          new Chart(this.dataChartToShowProductsBySales, {
+            type: 'polarArea',
+            data: {
+              labels: res.data.productNames,
+              datasets: [{
+                label: 'Total ',
+                data: res.data.selled,
+                borderWidth: 1
+              }]
+            },
+            options: {
+              scales: {
+                y: {
+                  beginAtZero: true
+                }
+              }
+            }
+          });
+        }
+
       })
   }
 
