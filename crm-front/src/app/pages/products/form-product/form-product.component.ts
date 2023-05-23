77f03118ca0f82c8efef8b8cdc0fdb01557fa798
santiagoSS99 <santiagoxq@icloud.com>
2023-05-22 21:53:47 -0500

    User can be provided purchase price -> this to calculate revenue

--- crm-front/src/app/pages/products/form-product/form-product.component.ts
+++ crm-front/src/app/pages/products/form-product/form-product.component.ts
@@ -16,6 +16,7 @@ import swal from 'sweetalert2'
 export class FormProductComponent implements OnInit {
   product: Product = {
     product_name: '',
+    purchasePrice: 0,
     price: 0,
     description: '',
     stock: 0,
@@ -77,6 +78,7 @@ export class FormProductComponent implements OnInit {
                   let product = {
                     "product_name": this.product.product_name,
                     "price": this.product.price,
+                    "purchasePrice": this.product.purchasePrice,
                     "description": this.product.description,
                     "stock": this.product.stock,
                     "images": images_test,
