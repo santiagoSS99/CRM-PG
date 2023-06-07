399f1df1316c8c2a1d1d4dd1406d74ad8d9cd8cd
santiagoSS99 <santiagoxq@icloud.com>
2023-06-06 22:41:16 -0500

    New dashboard to display performance about the bussines

--- crm-front/src/app/pages/dashboard/dashboard.component.ts
+++ crm-front/src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js';
+import { CustomerService } from 'src/app/services/customer.service';
+import { ProductService } from 'src/app/services/product.service';
 import { PurchaseLinesService } from 'src/app/services/purchase-lines.service';
+import { PurchaseService } from 'src/app/services/purchase.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,28 +16,93 @@ export class DashboardComponent implements OnInit {
   labels: any;
   chartProductCustomer: any
   quantity = [];
+  totalInvestment: any
+  totalCustomer: any
+
+  year: any
+  month: any
+  dataChartToShowAmount: any
+  dataChartToShowProductsBySales: any
+  productsLabels: Array<any> = []
 
   constructor(
-    private purchaseLineService: PurchaseLinesService
+    private purchaseLineService: PurchaseLinesService,
+    private customerService: CustomerService,
+    private purchaseService: PurchaseService,
+    private productService: ProductService,
   ) { }
 
   ngOnInit(): void {
-    this.dashboardCustomerProduct()
+    // this.dashboardCustomerProduct()
+    this.getTotalInvestment()
+    this.getTotalCustomers()
+    this.loadTotalAmountPurchase();
+    this.productsWithSales()
   }
 
-  dashboardCustomerProduct() {
-    this.purchaseLineService.getdataByCustomerProduct(this.token).subscribe(
+  // dashboardCustomerProduct() {
+  //   this.purchaseLineService.getdataByCustomerProduct(this.token).subscribe(
+  //     res => {
+  //       console.log(res)
+  //       if (res) {
+  //         this.chartProductCustomer = document.getElementById('myChart1');
+  //         new Chart(this.chartProductCustomer, {
+  //           type: 'line',
+  //           data: {
+  //             labels: res.customerName,
+  //             datasets: [{
+  //               label: 'Total ',
+  //               data: res.totalQuantity,
+  //               borderWidth: 1
+  //             }]
+  //           },
+  //           options: {
+  //             plugins: {
+  //               title: {
+  //                 display: true,
+  //                 text: 'Ventas por mes',
+  //                 padding: {
+  //                   top: 10,
+  //                   bottom: 30
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         });
+  //       }
+  //     });
+  // }
+
+  getTotalInvestment() {
+    this.productService.getTotalInvestment().subscribe(
       res => {
         console.log(res)
+        this.totalInvestment = res
+      }
+    )
+  }
+
+  getTotalCustomers() {
+    this.customerService.getTotalCustomers().subscribe(
+      res => {
+        this.totalCustomer = res
+      }
+    )
+  }
+
+  loadTotalAmountPurchase() {
+    this.purchaseService.getDataAmountToDash(this.token).subscribe(
+      res => {
+
         if (res) {
-          this.chartProductCustomer = document.getElementById('myChart1');
-          new Chart(this.chartProductCustomer, {
+          this.dataChartToShowAmount = document.getElementById('myChart1');
+          new Chart(this.dataChartToShowAmount, {
             type: 'line',
             data: {
-              labels: res.customerName,
+              labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dec'],
               datasets: [{
                 label: 'Total ',
-                data: res.totalQuantity,
+                data: res.data,
                 borderWidth: 1
               }]
             },
@@ -52,7 +120,54 @@ export class DashboardComponent implements OnInit {
             }
           });
         }
-      });
+
+        console.log(res)
+        console.log(res.data)
+      })
   }
+
+  productsWithSales() {
+    this.productService.productsWithSales(this.token).subscribe(
+      res => {
+        console.log(res.data.productNames);
+        this.productsLabels = res
+
+        if (res) {
+          this.dataChartToShowProductsBySales = document.getElementById('myChart3');
+          new Chart(this.dataChartToShowProductsBySales, {
+            type: 'doughnut',
+            data: {
+              labels: res.data.productNames,
+              datasets: [{
+                label: 'Total ',
+                data: res.data.selled,
+                borderWidth: 1
+              }]
+            },
+            options: {
+              plugins: {
+                legend: {
+                  position: 'bottom',
+                  display: true,
+                  labels: {
+                    textAlign: 'center'
+                  }
+                },
+                title: {
+                  display: true,
+                  text: 'Productos con mas demanda',
+                  padding: {
+                    top: 10,
+                    bottom: 30
+                  }
+                }
+              }
+            }
+          });
+        }
+
+      })
+  }
+
 
 }
