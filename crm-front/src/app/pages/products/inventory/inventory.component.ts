0e585649495c1780c0e5cec26601ebe1f5af3fae
santiagoSS99 <santiagoxq@icloud.com>
2023-06-13 18:36:23 -0500

    New function to delete products

--- crm-front/src/app/pages/products/inventory/inventory.component.ts
+++ crm-front/src/app/pages/products/inventory/inventory.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
 import { ProductService } from 'src/app/services/product.service';
 import { Subscription } from 'rxjs';
+import Swal from 'sweetalert2';
+import { text } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-inventory',
@@ -18,7 +20,7 @@ export class InventoryComponent implements OnInit {
   selectedProduct: any = {};
   subscription: Subscription;
 
-  constructor(private productService: ProductService) { 
+  constructor(private productService: ProductService) {
     this.subscription = this.productService.products.subscribe((prods) => {
       this.products = prods;
       this.products_const = prods;
@@ -56,6 +58,28 @@ export class InventoryComponent implements OnInit {
       this.products = this.products_const.filter((item: { product_name: string; }) => term.test(item.product_name))
     } else {
       this.products = this.products_const
+    }
+  }
+
+  removeProduct(id: any) {
+    console.log(id, 'im product id')
+    if (id) {
+      Swal.fire({
+        title: '¿Estás seguro de eliminar este producto?',
+        text: 'se eliminará de manera permanente este producto',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Sí, eliminar',
+        cancelButtonText: 'Cancelar'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.productService.deleteProduct(id).subscribe(
+            res => {
+              console.log(res)
+            }
+          )
+        }
+      })
     }
   }
 }
